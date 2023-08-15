a4aab7831ab728cf15e26145207f6a5ee7ef1530
cassiejc <133578041+cassiejc@users.noreply.github.com>
2023-08-15 23:14:08 +0700

    Add files via upload

---
+++ Window_Graph.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class Window_Graph : MonoBehaviour 
+{
+
+    [SerializeField] private Sprite circleSprite;
+    [SerializeField] private RectTransform graphContainer;
+    [SerializeField] private RectTransform labelTemplateX;
+    [SerializeField] private RectTransform labelTemplateY;
+    [SerializeField] private RectTransform dashTemplateX;
+    [SerializeField] private RectTransform dashTemplateY;
+    [SerializeField] private mqttReceiver _eventsender;
+    private List<GameObject> gameObjectList;
+
+    // public mqttReceiver _eventsender;
+    public int pointData;
+
+    private void Awake() {
+
+        // _eventsender = FindObjectOfType<mqttReceiver>();
+
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
+        dashTemplateX = graphContainer.Find("dashTemplateX").GetComponent<RectTransform>();
+        dashTemplateY = graphContainer.Find("dashTemplateY").GetComponent<RectTransform>();
+
+        gameObjectList = new List<GameObject>();
+
+        List<int> valueList = new List<int>() ;
+        // { 
+        //     // 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        //     // 5, 98, 56, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33 
+        //     // int.Parse(receiver.OnMessageArrived += message)
+        // };
+        // // ShowGraph(valueList, (int _i) => "Day " + (_i + 1), (float _f) => "$" + Mathf.RoundToInt(_f));
+        // ShowGraph(valueList, -1, (int _i) => "" + (_i + 1), (float _f) => "" + Mathf.RoundToInt(_f));
+
+        FunctionPeriodic.Create(() => {
+            // valueList.RemoveAt(0);
+            valueList.Add(pointData);
+            string result = "List contents: ";
+            foreach (var item in valueList)
+            {
+                result += item.ToString() + ", ";
+            }
+            Debug.Log(result);
+            ShowGraph(valueList, -1, (int _i) => "" + (_i), (float _f) => "" + Mathf.RoundToInt(_f));
+        }, .5f);
+    }
+
+    void Start()
+    {
+        _eventsender.OnMessageArrived += OnMessageArrivedHandler;
+    }
+
+    private void OnMessageArrivedHandler(string newMsg)
+    {
+        pointData = decimal.ToInt32(decimal.Parse(newMsg)); 
+        Debug.Log("The point data is : " + pointData);
+    }
+
+    private void ShowGraph(List<int> valueList, int maxVisibleValueAmount = -1, Func<int, string> getAxisLabelX = null, Func<float, string> getAxisLabelY = null) {
+        if (getAxisLabelX == null) {
+            getAxisLabelX = delegate (int _i) { return _i.ToString(); };
+        }
+        if (getAxisLabelY == null) {
+            getAxisLabelY = delegate (float _f) { return Mathf.RoundToInt(_f).ToString(); };
+        }
+        if (maxVisibleValueAmount <= 0) {
+            maxVisibleValueAmount = valueList.Count;
+        }
+
+        foreach (GameObject gameObject in gameObjectList) {
+            Destroy(gameObject);
+        }
+        gameObjectList.Clear();
+
+        float graphWidth = graphContainer.sizeDelta.x;
+        float graphHeight = graphContainer.sizeDelta.y;
+
+        float yMaximum = valueList[0];
+        float yMinimum = valueList[0];
+
+        for (int i = Mathf.Max(valueList.Count - maxVisibleValueAmount, 0); i < valueList.Count; i++) {
+            int value = valueList[i];
+            if (value > yMaximum) {
+                yMaximum = value;
+            }
+            if (value < yMinimum) {
+                yMinimum = value;
+            }
+        }
+
+        float yDifference = yMaximum - yMinimum;
+        if (yDifference <= 0) {
+            yDifference = 5f;
+        }
+
+        yMaximum = yMaximum + ((yMaximum - yMinimum) * 0.2f);
+        yMinimum = yMinimum - ((yMaximum - yMinimum) * 0.2f);
+
+        yMinimum = 0f; // Start the graph at zero
+
+        float xSize = graphWidth / (maxVisibleValueAmount + 1);
+
+        int xIndex = 0;
+
+        GameObject lastCircleGameObject = null;
+        for (int i = Mathf.Max(valueList.Count - maxVisibleValueAmount, 0); i < valueList.Count; i++) {
+            float xPosition = xSize + i * xSize;
+            float yPosition = ((valueList[i] - yMinimum) / (yMaximum - yMinimum)) * graphHeight;
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            gameObjectList.Add(circleGameObject);
+            if (lastCircleGameObject != null) {
+                GameObject dotConnectionGameObject = CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+                gameObjectList.Add(dotConnectionGameObject);
+            }
+            lastCircleGameObject = circleGameObject;
+
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer, false);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition = new Vector2(xPosition, -55f);
+            labelX.GetComponent<Text>().text = getAxisLabelX(i);
+            gameObjectList.Add(labelX.gameObject);
+
+            RectTransform dashX = Instantiate(dashTemplateX);
+            dashX.SetParent(graphContainer, false);
+            dashX.gameObject.SetActive(true);
+            dashX.anchoredPosition = new Vector2(xPosition, -3f);
+            gameObjectList.Add(dashX.gameObject);
+        }
+
+        int separatorCount = 10;
+        for (int i = 0; i <= separatorCount; i++) {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer, false);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = i * 1f / separatorCount;
+            labelY.anchoredPosition = new Vector2(25f, normalizedValue * graphHeight);
+            labelY.GetComponent<Text>().text = getAxisLabelY(yMinimum + (normalizedValue * (yMaximum - yMinimum)));
+            gameObjectList.Add(labelY.gameObject);
+
+            RectTransform dashY = Instantiate(dashTemplateY);
+            dashY.SetParent(graphContainer, false);
+            dashY.gameObject.SetActive(true);
+            dashY.anchoredPosition = new Vector2(-4f, normalizedValue * graphHeight);
+            gameObjectList.Add(dashY.gameObject);
+        }
+    }
+
+    private GameObject CreateCircle(Vector2 anchoredPosition) {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    private GameObject CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB) {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+        return gameObject;
+    }
+
+}
